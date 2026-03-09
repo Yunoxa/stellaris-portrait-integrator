58284a2c1b3576be8752a370880d8f5a2bb05b2d
Yunoxa <agentreborn3@gmail.com>
2026-03-09 23:37:07 +0000

    Initial setup

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ process.stdin.on('data', (data) => {
   const paths = {};
   paths.root = data.toString().trim();
   paths.models = `${paths.root}gfx/models/portraits/`
-  console.log(`Your root mod path: ${paths.root}`);
-  console.log(fs.readdirSync(paths.root));
-  console.log(fs.readdirSync(paths.models))
+
+  // Get portrait category directories
+  const modelDirFiles = fs.readdirSync(paths.models, { withFileTypes: true });
+  const modelDirs = modelDirFiles.filter(file => file.isDirectory);
+
+  modelDirs.forEach((dir) => {
+    const modelDirContent = fs.readdirSync(dir.parentPath+dir.name, { withFileTypes: true });
+    const portraitGroupDirs = modelDirContent.filter(file => file.isDirectory);
+
+    // Get dds files in portrait group directory.
+    portraitGroupDirs.forEach((dir) => {
+      const portraitGroupContent = fs.readdirSync(`${dir.parentPath}/${dir.name}`, { withFileTypes: true });
+      console.log(portraitGroupContent)
+      const portraitGroupImages = portraitGroupContent.filter(file => file.name.substring(file.name.length - 3) == "dds");
+      console.log(portraitGroupImages)
+    });
+  });
+
   process.exit();
 });
